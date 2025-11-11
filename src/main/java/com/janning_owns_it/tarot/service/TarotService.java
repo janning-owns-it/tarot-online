643e62cc86a938ddcaf0b5b1fa8cfca578a7f06a
Lucas Janning <lucasjanning@Lucass-MacBook-Air.local>
2025-11-11 23:26:14 +0000

    First commit of OpenAI integration

--- src/main/java/com/janning_owns_it/tarot/service/TarotService.java
+++ src/main/java/com/janning_owns_it/tarot/service/TarotService.java
@@ -2,9 +2,15 @@ package com.janning_owns_it.tarot.service;
 
 import com.janning_owns_it.tarot.component.PromptManager;
 import com.janning_owns_it.tarot.model.TarotReadingResponse;
+import com.openai.client.OpenAIClient;
+import com.openai.client.okhttp.OpenAIOkHttpClient;
+import com.openai.models.ChatModel;
+import com.openai.models.chat.completions.ChatCompletion;
+import com.openai.models.chat.completions.ChatCompletionCreateParams;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.Set;
 
 @Service
@@ -31,7 +37,23 @@ public class TarotService {
     }
 
     private String askToArcaneGuide(String querentsQuestion, String sortedCardsToTextInOrder) throws IOException {
-        new PromptManager().getPrompts(querentsQuestion, sortedCardsToTextInOrder);
-        return querentsQuestion + "\n" + sortedCardsToTextInOrder;
+        OpenAIClient openAIClient = OpenAIOkHttpClient.builder()
+                .apiKey("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")
+                .build();
+        Map<String, String> promptManager = new PromptManager().getPrompts(querentsQuestion, sortedCardsToTextInOrder);
+
+        ChatCompletionCreateParams params = ChatCompletionCreateParams.builder()
+                .model(ChatModel.of("gpt-3.5-turbo"))
+                .addSystemMessage(promptManager.get("system"))
+                .addUserMessage(promptManager.get("user"))
+                .maxTokens(1000)
+                .build();
+
+        ChatCompletion completion = openAIClient.chat().completions().create(params);
+
+        return completion.choices().stream()
+                    .flatMap(choice -> choice.message().content().stream())
+                    .findFirst()
+                    .orElse("No response from Arcane Guide");
     }
 }
